74430ae7a9ab28d3a9835367e04aeebaac0c7e86
Khoi Trinh <johntrinhnhat@gmail.com>
2023-08-13 12:08:33 -0400

    update: NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="logo-container">
+          <div className="logo">
+            <div className="echo">Echo</div>
+          </div>
+          <div className="headphone-icon"></div>
+        </div>
+        <button type="button" className="cart-icon">
+          <AiOutlineShopping />
+          <span className="cart-item-qty">1</span>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
