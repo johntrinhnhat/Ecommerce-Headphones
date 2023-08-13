496e87ad3ae3ada4780e9df62497a9d3cb9b46dd
Khoi Trinh <johntrinhnhat@gmail.com>
2023-08-13 13:32:18 -0400

    update: FooterBanner comp

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import HeroBanner from "@/components/HeroBanner";
 import React from "react";
 import { getProduct, getBanner } from "@/sanity-headphone/sanity.utils";
 import Product from "@/components/Product";
+import FooterBanner from "@/components/FooterBanner";
 
 const Home = async () => {
   const bannerData = await getBanner();
@@ -18,6 +19,7 @@ const Home = async () => {
             <Product key={product._id} product={product} />
           ))}
         </div>
+        <FooterBanner footerBanner={bannerData.length && bannerData[0]} />
       </main>
     </>
   );
