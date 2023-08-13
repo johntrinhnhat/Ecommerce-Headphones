0986cbe1dfeba46a970bf381d1bd9c131e9ef7f4
Khoi Trinh <johntrinhnhat@gmail.com>
2023-08-13 12:19:33 -0400

    update: HeroBanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import Link from "next/link";
 
-const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+const HeroBanner = ({}) => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">smallText</p>
+        <h3>midText</h3>
+        <h1>largeText1</h1>
+        <img src="" alt="headphones" className="hero-banner-image"></img>
+      </div>
+      <div>
+        <Link href="/product/ID">
+          <button type="button">buttonText</button>
+        </Link>
+        <div className="desc">
+          <h5>Description</h5>
+          <p>desc</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
